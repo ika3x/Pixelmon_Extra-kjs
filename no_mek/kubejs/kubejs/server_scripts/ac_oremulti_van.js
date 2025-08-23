f55af2f3ca68739839dd8b3f316cd6d6549d6aae
ika3x <squidikasan@gmail.com>
2025-08-23 14:38:23 +0000

    OreFix(not compl)

--- no_mek/kubejs/kubejs/server_scripts/ac_oremulti_van.js
+++ no_mek/kubejs/kubejs/server_scripts/ac_oremulti_van.js
@@ -1,201 +1,201 @@
 onEvent('recipes', event => {
-// (3, 1)
-event.shapeless('3x kubejs:dust_fe', [
-  'minecraft:iron_ore',
-  'kubejs:apricore1'
-]);
-event.shapeless('3x kubejs:dust_au', [
-  'minecraft:gold_ore',
-  'kubejs:apricore1'
-]);
-event.shapeless('3x kubejs:dust_diamond', [
-  'minecraft:diamond_ore',
-  'kubejs:apricore1'
-]);
-event.shapeless('3x kubejs:dust_coal', [
-  'minecraft:coal_ore',
-  'kubejs:apricore1'
-]);
-event.shapeless('3x kubejs:dust_sio2_nether', [
-  'minecraft:nether_quartz_ore',
-  'kubejs:apricore1'
-]);
-event.shapeless('3x kubejs:dust_be3al2si6o18', [
-  'minecraft:emerald_ore',
-  'kubejs:apricore1'
-]);
-event.shapeless('9x minecraft:redstone', [
-  'minecraft:redstone_ore',
-  'kubejs:apricore1'
-]);
-event.shapeless('9x minecraft:lapis_lazuli', [
-  'minecraft:lapis_ore',
-  'kubejs:apricore1'
-]);
-
-// (6, 2)
-event.shapeless('6x kubejs:dust_fe', [
-  'minecraft:iron_ore',
-  'kubejs:apricore2'
-]);
-event.shapeless('6x kubejs:dust_au', [
-  'minecraft:gold_ore',
-  'kubejs:apricore2'
-]);
-event.shapeless('6x kubejs:dust_diamond', [
-  'minecraft:diamond_ore',
-  'kubejs:apricore2'
-]);
-event.shapeless('6x kubejs:dust_coal', [
-  'minecraft:coal_ore',
-  'kubejs:apricore2'
-]);
-event.shapeless('8x kubejs:dust_sio2_nether', [
-  'minecraft:nether_quartz_ore',
-  'kubejs:apricore2'
-]);
-event.shapeless('6x kubejs:dust_be3al2si6o18', [
-  'minecraft:emerald_ore',
-  'kubejs:apricore2'
-]);
-event.shapeless('16x minecraft:redstone', [
-  'minecraft:redstone_ore',
-  'kubejs:apricore2'
-]);
-event.shapeless('16x minecraft:lapis_lazuli', [
-  'minecraft:lapis_ore',
-  'kubejs:apricore2'
-]);
-
-// (10, 3)
-event.shapeless('10x kubejs:dust_fe', [
-  'minecraft:iron_ore',
-  'kubejs:apricore3'
-]);
-event.shapeless('10x kubejs:dust_au', [
-  'minecraft:gold_ore',
-  'kubejs:apricore3'
-]);
-event.shapeless('10x kubejs:dust_diamond', [
-  'minecraft:diamond_ore',
-  'kubejs:apricore3'
-]);
-event.shapeless('10x kubejs:dust_coal', [
-  'minecraft:coal_ore',
-  'kubejs:apricore3'
-]);
-event.shapeless('12x kubejs:dust_sio2_nether', [
-  'minecraft:nether_quartz_ore',
-  'kubejs:apricore3'
-]);
-event.shapeless('10x kubejs:dust_be3al2si6o18', [
-  'minecraft:emerald_ore',
-  'kubejs:apricore3'
-]);
-event.shapeless('42x minecraft:redstone', [
-  'minecraft:redstone_ore',
-  'kubejs:apricore3'
-]);
-event.shapeless('42x minecraft:lapis_lazuli', [
-  'minecraft:redstone_ore',
-  'kubejs:apricore3'
-]);
-
-// (24, 4)
-event.shapeless('24x kubejs:dust_fe', [
-  'minecraft:iron_ore',
-  'kubejs:apricore4'
-]);
-event.shapeless('24x kubejs:dust_au', [
-  'minecraft:gold_ore',
-  'kubejs:apricore4'
-]);
-event.shapeless('24x kubejs:dust_diamond', [
-  'minecraft:diamond_ore',
-  'kubejs:apricore4'
-]);
-event.shapeless('24x kubejs:dust_coal', [
-  'minecraft:coal_ore',
-  'kubejs:apricore4'
-]);
-event.shapeless('32x kubejs:dust_sio2_nether', [
-  'minecraft:nether_quartz_ore',
-  'kubejs:apricore4'
-]);
-event.shapeless('24x kubejs:dust_be3al2si6o18', [
-  'minecraft:emerald_ore',
-  'kubejs:apricore4'
-]);
-event.shapeless('10x minecraft:redstone_block', [
-  'minecraft:redstone_ore',
-  'kubejs:apricore4'
-]);
-event.shapeless('10x minecraft:lapis_block', [
-  'minecraft:lapis_ore',
-  'kubejs:apricore4'
-]);
-
-// (64, 5)
-event.shapeless('64x kubejs:dust_fe', [
-  'minecraft:iron_ore',
-  'kubejs:apricore5'
-]);
-event.shapeless('64x kubejs:dust_au', [
-  'minecraft:gold_ore',
-  'kubejs:apricore5'
-]);
-event.shapeless('64x kubejs:dust_diamond', [
-  'minecraft:diamond_ore',
-  'kubejs:apricore5'
-]);
-event.shapeless('64x kubejs:dust_coal', [
-  'minecraft:coal_ore',
-  'kubejs:apricore5'
-]);
-event.shapeless('64x kubejs:dust_sio2_nether', [
-  'minecraft:nether_quartz_ore',
-  'kubejs:apricore5'
-]);
-event.shapeless('64x kubejs:dust_be3al2si6o18', [
-  'minecraft:emerald_ore',
-  'kubejs:apricore5'
-]);
-event.shapeless('64x minecraft:redstone', [
-  'minecraft:redstone_ore',
-  'kubejs:apricore5'
-]);
-event.shapeless('64x minecraft:lapis_lazuli', [
-  'minecraft:lapis_ore',
-  'kubejs:apricore5'
-]);
-
-// (99, 6)
-event.shapeless('24x minecraft:iron_block', [
-  'minecraft:iron_ore',
-  'kubejs:apricore6'
-]);
-event.shapeless('24x minecraft:gold_block', [
-  'minecraft:gold_ore',
-  'kubejs:apricore6'
-]);
-event.shapeless('24x minecraft:diamond_block', [
-  'minecraft:diamond_ore',
-  'kubejs:apricore6'
-]);
-event.shapeless('24x minecraft:coal_block', [
-  'minecraft:coal_ore',
-  'kubejs:apricore6'
+ event.shapeless('4x kubejs:dust_al', [
+    'pixelmon:bauxite_ore',
+    'kubejs:apricore1'
   ]);
-event.shapeless('24x minecraft:emerald_block', [
-  'minecraft:emerald_ore',
+   event.shapeless('4x kubejs:dust_ag', [
+    'pixelmon:silver_ore',
+    'kubejs:apricore1'
+  ]);
+ event.shapeless('4x kubejs:dust_pt', [
+    'pixelmon:platinum_ore',
+    'kubejs:apricore1'
+  ]);
+  event.shapeless('4x kubejs:dust_si', [
+    'pixelmon:silicon_ore',
+    'kubejs:apricore1'
+  ]);
+  event.shapeless('4x kubejs:dust_al2o3_red', [
+    'pixelmon:ruby_ore',
+    'kubejs:apricore1'
+  ]);
+  event.shapeless('4x kubejs:dust_al2o3_blue', [
+    'pixelmon:sapphire_ore',
+    'kubejs:apricore1'
+  ]);
+  event.shapeless('4x kubejs:dust_sio2_purple', [
+    'pixelmon:amethyst_ore',
+    'kubejs:apricore1'
+  ]);
+  event.shapeless('4x kubejs:dust_sio2_white', [
+    'pixelmon:crystal_ore',
+    'kubejs:apricore1'
+  ]);
+  event.shapeless('9x kubejs:dust_al', [
+  'pixelmon:bauxite_ore',
+  'kubejs:apricore2'
+]);
+event.shapeless('9x kubejs:dust_ag', [
+  'pixelmon:silver_ore',
+  'kubejs:apricore2'
+]);
+event.shapeless('9x kubejs:dust_pt', [
+  'pixelmon:platinum_ore',
+  'kubejs:apricore2'
+]);
+event.shapeless('9x kubejs:dust_si', [
+  'pixelmon:silicon_ore',
+  'kubejs:apricore2'
+]);
+event.shapeless('9x kubejs:dust_al2o3_red', [
+  'pixelmon:ruby_ore',
+  'kubejs:apricore2'
+]);
+event.shapeless('9x kubejs:dust_al2o3_blue', [
+  'pixelmon:sapphire_ore',
+  'kubejs:apricore2'
+]);
+event.shapeless('9x kubejs:dust_sio2_purple', [
+  'pixelmon:amethyst_ore',
+  'kubejs:apricore2'
+]);
+event.shapeless('9x kubejs:dust_sio2_white', [
+  'pixelmon:crystal_ore',
+  'kubejs:apricore2'
+]);
+// (32, 3)
+event.shapeless('32x kubejs:dust_al', [
+  'pixelmon:bauxite_ore',
+  'kubejs:apricore3'
+]);
+event.shapeless('32x kubejs:dust_ag', [
+  'pixelmon:silver_ore',
+  'kubejs:apricore3'
+]);
+event.shapeless('32x kubejs:dust_pt', [
+  'pixelmon:platinum_ore',
+  'kubejs:apricore3'
+]);
+event.shapeless('32x kubejs:dust_si', [
+  'pixelmon:silicon_ore',
+  'kubejs:apricore3'
+]);
+event.shapeless('32x kubejs:dust_al2o3_red', [
+  'pixelmon:ruby_ore',
+  'kubejs:apricore3'
+]);
+event.shapeless('32x kubejs:dust_al2o3_blue', [
+  'pixelmon:sapphire_ore',
+  'kubejs:apricore3'
+]);
+event.shapeless('32x kubejs:dust_sio2_purple', [
+  'pixelmon:amethyst_ore',
+  'kubejs:apricore3'
+]);
+event.shapeless('32x kubejs:dust_sio2_white', [
+  'pixelmon:crystal_ore',
+  'kubejs:apricore3'
+]);
+
+// (144, 4)
+event.shapeless('16x pixelmon:aluminium_block', [
+  'pixelmon:bauxite_ore',
+  'kubejs:apricore4'
+]);
+event.shapeless('16x pixelmon:silver_block', [
+  'pixelmon:silver_ore',
+  'kubejs:apricore4'
+]);
+event.shapeless('16x pixelmon:platinum_block', [
+  'pixelmon:platinum_ore',
+  'kubejs:apricore4'
+]);
+event.shapeless('16x pixelmon:silicon_block', [
+  'pixelmon:silicon_ore',
+  'kubejs:apricore4'
+]);
+event.shapeless('16x pixelmon:ruby_block', [
+  'pixelmon:ruby_ore',
+  'kubejs:apricore4'
+]);
+event.shapeless('16x pixelmon:sapphire_block', [
+  'pixelmon:sapphire_ore',
+  'kubejs:apricore4'
+]);
+event.shapeless('16x pixelmon:amethyst_block', [
+  'pixelmon:amethyst_ore',
+  'kubejs:apricore4'
+]);
+event.shapeless('16x pixelmon:crystal_block', [
+  'pixelmon:crystal_ore',
+  'kubejs:apricore4'
+]);
+
+// (144, 5)
+event.shapeless('64x pixelmon:aluminium_block', [
+  'pixelmon:bauxite_ore',
+  'kubejs:apricore5'
+]);
+event.shapeless('64x pixelmon:silver_block', [
+  'pixelmon:silver_ore',
+  'kubejs:apricore5'
+]);
+event.shapeless('64x pixelmon:platinum_block', [
+  'pixelmon:platinum_ore',
+  'kubejs:apricore5'
+]);
+event.shapeless('64x pixelmon:silicon_block', [
+  'pixelmon:silicon_ore',
+  'kubejs:apricore5'
+]);
+event.shapeless('64x pixelmon:ruby_block', [
+  'pixelmon:ruby_ore',
+  'kubejs:apricore5'
+]);
+event.shapeless('64x pixelmon:sapphire_block', [
+  'pixelmon:sapphire_ore',
+  'kubejs:apricore5'
+]);
+event.shapeless('64x pixelmon:amethyst_block', [
+  'pixelmon:amethyst_ore',
+  'kubejs:apricore5'
+]);
+event.shapeless('64x pixelmon:crystal_block', [
+  'pixelmon:crystal_ore',
+  'kubejs:apricore5'
+]);
+
+// (576, 6)
+event.shapeless('64x pixelmon:aluminium_block', [
+  'pixelmon:bauxite_ore',
   'kubejs:apricore6'
 ]);
-event.shapeless('64x minecraft:redstone_block', [
-  'minecraft:redstone_ore',
+event.shapeless('64x pixelmon:silver_block', [
+  'pixelmon:silver_ore',
   'kubejs:apricore6'
 ]);
-event.shapeless('64x minecraft:lapis_block', [
-  'minecraft:lapis_ore',
+event.shapeless('64x pixelmon:platinum_block', [
+  'pixelmon:platinum_ore',
+  'kubejs:apricore6'
+]);
+event.shapeless('64x pixelmon:silicon_block', [
+  'pixelmon:silicon_ore',
+  'kubejs:apricore6'
+]);
+event.shapeless('64x pixelmon:ruby_block', [
+  'pixelmon:ruby_ore',
+  'kubejs:apricore6'
+]);
+event.shapeless('64x pixelmon:sapphire_block', [
+  'pixelmon:sapphire_ore',
+  'kubejs:apricore6'
+]);
+event.shapeless('64x pixelmon:amethyst_block', [
+  'pixelmon:amethyst_ore',
+  'kubejs:apricore6'
+]);
+event.shapeless('64x pixelmon:crystal_block', [
+  'pixelmon:crystal_ore',
   'kubejs:apricore6'
 ]);
 });
